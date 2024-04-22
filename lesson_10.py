a8455a79f21fac2613057bf61707855efae48732
Vladimir <makrosdv@gmail.com>
2024-04-22 23:47:19 +0300

    Bla bla

--- lesson_10.py
+++ lesson_10.py
@@ -10,3 +10,14 @@ def capital_game():
         'Finland': 'Helsinki', 'Poland': 'Warsaw',
         'Romania': 'Bucharest', 'Bulgaria': 'Sofia', 'Greece': 'Athens'
     }
+    
+    def capital_game():
+    capitals = {
+        'Ukraine': 'Kyiv', 'France': 'Paris', 'Germany': 'Berlin',
+        'Italy': 'Rome', 'USA': 'Washington', 'Canada': 'Ottawa',
+        'Switzerland': 'Bern', 'Austria': 'Vienna',
+        'Belgium': 'Brussels',  'Sweden': 'Stockholm',
+        'Norway': 'Oslo', 'Denmark': 'Copenhagen',
+        'Finland': 'Helsinki', 'Poland': 'Warsaw',
+        'Romania': 'Bucharest', 'Bulgaria': 'Sofia', 'Greece': 'Athens'
+    }
