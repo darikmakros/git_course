9013fea3049e4268a1b85f638b29cb80e7823cad
Vladimir <makrosdv@gmail.com>
2024-04-22 23:34:04 +0300

    second status

---
+++ lesson_10.py
@@ -0,0 +1,12 @@
+import random
+
+def capital_game():
+    capitals = {
+        'Ukraine': 'Kyiv', 'France': 'Paris', 'Germany': 'Berlin',
+        'Italy': 'Rome', 'USA': 'Washington', 'Canada': 'Ottawa',
+        'Switzerland': 'Bern', 'Austria': 'Vienna',
+        'Belgium': 'Brussels',  'Sweden': 'Stockholm',
+        'Norway': 'Oslo', 'Denmark': 'Copenhagen',
+        'Finland': 'Helsinki', 'Poland': 'Warsaw',
+        'Romania': 'Bucharest', 'Bulgaria': 'Sofia', 'Greece': 'Athens'
+    }
