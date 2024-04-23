e96e00b8cfbe5af7431b67bd78801fb0efd27750
Vladimir <makrosdv@gmail.com>
2024-04-23 11:40:12 +0300

    Refactor getHatCats function in lesson_10.py

--- lesson_10.py
+++ lesson_10.py
@@ -1,15 +1,19 @@
-def getHatCats(numCats, numRounds):
-    #INITIALIZE hats[numCats] to [False, False, ..., False] // массив длиной numCats
-    hats = [False] * numCats
-    #FOR roundNum in range(numRounds)
-    for roundNum in range(numRounds):
-        #FOR catNum in range(numCats)
-        for catNum in range(numCats):
-            #IF catNum is divisible by roundNum
-            if catNum % roundNum == 0:
-                #TOGGLE hats[catNum]
-                hats[catNum] = not hats[catNum]
-    #RETURN hats
-    return hats
+def getHatCats(num_cats, num_rounds):
+    # Инициализация массива, где каждый элемент представляет кошку.
+    # Значение False означает, что на кошке нет шапки.
+    hats = [False] * num_cats
 
-getHatCats(100, 100)
+    # Проходим по каждому кругу
+    for i in range(1, num_rounds + 1):
+        # На каждом i-ом круге переключаем состояние шапки на каждой i-ой кошке
+        for j in range(i - 1, num_cats, i):
+            hats[j] = not hats[j]
+
+    # Формирование списка индексов кошек, у которых остались шапки
+    hatted_cats = [index + 1 for index, has_hat in enumerate(hats) if has_hat]
+    return hatted_cats
+
+# Тестирование функции с 100 кошками и 100 кругами
+hatted_cats = getHatCats(100, 100)
+print("Cats with hats:", hatted_cats)
+
